a8f2f2557979a26332928c8d769b946284b9c541
Tuan3_PhamHuynhTin <huynhtin0501@gmail.com>
2021-11-15 11:11:15 +0700

    Day 03

--- js/IndexAdmin.js
+++ js/IndexAdmin.js
@@ -1,1 +1,20 @@
 
+var x = document.getElementById("login");
+var y = document.getElementById("forgot");
+var z = document.getElementById("check-email");
+function swap(){
+    x.style.display = "none";
+    y.style.display = "block";
+}
+function swapBack(){
+    y.style.display = "none";
+    x.style.display = "block";
+}
+function swapContinue(){
+    y.style.display = "none";
+    z.style.display = "block";
+}
+function swapBackCheck(){
+    z.style.display = "none";
+    x.style.display = "block";
+}
